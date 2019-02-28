b6ee85ec32d1c3b24723478053efca670602e85b
Gazbond <dev@gazbond.co.uk>
2019-02-28 12:26:52 +0000

    Changed to using models for inserts.

--- seeds/users.js
+++ seeds/users.js
@@ -1,15 +1,14 @@
 exports.seed = async function(knex) {
+  const db = require("../models")(knex);
   await knex("user_identity").del();
-  await knex("user_identity").insert([
-    {
-      id: 1,
-      username: "root",
-      email: "dev@gazbond.co.uk"
-    },
-    {
-      id: 2,
-      username: "gazbond",
-      email: "gaz@gazbond.co.uk"
-    }
-  ]);
+  await db.User.query().insert({
+    username: "root",
+    email: "dev@gazbond.co.uk",
+    password_hash: "password"
+  });
+  await db.User.query().insert({
+    username: "gazbond",
+    email: "gaz@gazbond.co.uk",
+    password_hash: "password"
+  });
 };
