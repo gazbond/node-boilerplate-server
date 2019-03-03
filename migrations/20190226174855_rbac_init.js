fcd07d7730ea86ede1022712226319ba81c484bf
Gazbond <dev@gazbond.co.uk>
2019-03-03 22:42:38 +0000

    Added foreign key constraints.

--- migrations/20190226174855_rbac_init.js
+++ migrations/20190226174855_rbac_init.js
@@ -10,14 +10,19 @@ exports.up = async function(knex) {
     table.string("rule_name", 64);
     table.binary("data");
     table.timestamps();
+    table.foreign("rule_name").references("rbac_rule.name");
   });
   await knex.schema.createTable("rbac_item_child", function(table) {
     table.string("parent", 64);
     table.string("child", 64);
+    table.primary(["parent", "child"]);
+    table.foreign("parent").references("rbac_item.name");
+    table.foreign("child").references("rbac_item.name");
   });
   await knex.schema.createTable("rbac_assignment", function(table) {
     table.string("item_name");
     table.string("user_id");
+    table.foreign("item_name").references("rbac_item.name");
   });
 };
 
