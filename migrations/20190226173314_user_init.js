f1f68a7d2ff33b9fd1158fa23ce73b0af53bc508
Gazbond <dev@gazbond.co.uk>
2019-03-11 16:45:39 +0000

    Updated to use unsigned id and not null fields.

--- migrations/20190226173314_user_init.js
+++ migrations/20190226173314_user_init.js
@@ -1,17 +1,27 @@
+const Knex = require("knex");
+/**
+ * @param {Knex} knex
+ */
 exports.up = async function(knex) {
   await knex.schema.createTable("user_identity", function(table) {
-    table.increments();
-    table.string("username", 25);
-    table.string("email", 255);
+    table
+      .increments()
+      .unsigned()
+      .notNullable();
+    table.string("username", 25).notNullable();
+    table.string("email", 255).notNullable();
     table.string("password_hash", 60);
     table.string("auth_key", 32);
     table.timestamps();
   });
   await knex.schema.createTable("user_profile", function(table) {
-    table.increments();
+    table
+      .increments()
+      .unsigned()
+      .notNullable();
     table.string("phone", 25);
     table.string("bio", 255);
-    table.integer("user_id");
+    table.integer("user_id").notNullable();
     table.timestamps();
     table.foreign("user_id").references("user_identity.id");
   });
