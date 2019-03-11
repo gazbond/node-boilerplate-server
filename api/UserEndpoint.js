529fe989a19063c87dc6b069d4c31cfd0bb5e6ab
Gazbond <dev@gazbond.co.uk>
2019-03-11 20:16:36 +0000

    Added validation for query param.

--- api/UserEndpoint.js
+++ api/UserEndpoint.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const { check, validationResult } = require("express-validator/check");
+
 const User = require("../models/User");
 const BaseEndpoint = require("./BaseEndpoint");
 
@@ -7,11 +10,21 @@ module.exports = class UserEndpoint extends BaseEndpoint {
     // Options:
     this.route = "/users";
     this.routeWithId = "/users/:id";
+    // Validation:
+    this.queryValidation = [check("q").isJSON()];
   }
   initRoutes(router) {
     super.initRoutes(router);
+    const newRouter = express.Router();
+    // Replace routes:
+    newRouter.post(this.route, this.queryValidation, this.actionIndex);
   }
   async actionIndex(req, res) {
+    // Check validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
     const users = await this.Model.query();
     return res.status(200).send(users);
   }
