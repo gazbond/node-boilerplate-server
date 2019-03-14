f8747f15715b5a86572199ee412c3a1326a701ad
Gazbond <dev@gazbond.co.uk>
2019-03-14 20:51:29 +0000

    Refactored bass and user endpoints and added jwt/rbac auth handlers.

--- api/UserEndpoint.js
+++ api/UserEndpoint.js
@@ -1,41 +1,49 @@
-const express = require("express");
 const {
   buildCheckFunction,
   validationResult
 } = require("express-validator/check");
 const check = buildCheckFunction(["body", "query"]);
 
-const User = require("../models/User");
 const BaseEndpoint = require("../lib/BaseEndpoint");
+const User = require("../models/User");
+const passport = require("../lib/helpers/passport");
+const rbac = require("../lib/helpers/rbac");
 
+/**
+ * User endpoint exposes User model over http.
+ */
 module.exports = class UserEndpoint extends BaseEndpoint {
+  /**
+   * Configuration.
+   */
   constructor() {
     super(User);
     // Options:
     this.route = "/users";
     this.routeWithId = "/users/:id";
-    // Validation:
-    this.queryValidation = [check("q").isJSON()];
+    // Route handlers:
+    this.handlers = [passport.jwt.auth, rbac.init, rbac.auth.isInRole("admin")];
   }
-  initRoutes(router) {
-    super.initRoutes(router);
-    const newRouter = express.Router();
-    // Replace routes:
-    newRouter.post(this.route, this.queryValidation, this.actionIndex);
-  }
-  async actionIndex(req, res) {
+  /**
+   * Uses json param 'q'
+   */
+  async index(req, res, next) {
     // Check validation errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      // 400 Bad Request
+      return res.status(400).send(errors.mapped());
     }
     const users = await this.Model.query();
-    return res.status(200).send(users);
+    // 200 OK
+    res.status(200).send(users);
   }
-  async actionCreate(req, res) {
-    return res.status(501).end();
+  async create(req, res, next) {
+    // 501 Not Implemented
+    res.status(501).end();
   }
-  async actionUpdate(req, res) {
-    return res.status(501).end();
+  async update(req, res, next) {
+    // 501 Not Implemented
+    res.status(501).end();
   }
 };
