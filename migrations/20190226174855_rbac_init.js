17ad1719b644df28f36683c15b2481010b4ebd08
Gazbond <dev@gazbond.co.uk>
2019-03-14 22:38:54 +0000

    Updated migration to use knex reference.

--- migrations/20190226174855_rbac_init.js
+++ migrations/20190226174855_rbac_init.js
@@ -1,26 +1,30 @@
+const Knex = require("knex");
+/**
+ * @param {Knex} knex
+ */
 exports.up = async function(knex) {
   await knex.schema.createTable("rbac_role", function(table) {
     table
       .string("name", 64)
       .primary()
-      .notNull();
+      .notNullable();
   });
   await knex.schema.createTable("rbac_permission", function(table) {
     table
       .string("name", 64)
       .primary()
-      .notNull();
+      .notNullable();
   });
   await knex.schema.createTable("rbac_permission_assignment", function(table) {
-    table.string("permission_name", 64).notNull();
-    table.string("role_name", 64).notNull();
+    table.string("permission_name", 64).notNullable();
+    table.string("role_name", 64).notNullable();
     table.primary(["role_name", "permission_name"]);
     table.foreign("role_name").references("rbac_role.name");
     table.foreign("permission_name").references("rbac_permission.name");
   });
   await knex.schema.createTable("rbac_role_assignment", function(table) {
-    table.string("role_name", 64).notNull();
-    table.integer("user_id").notNull();
+    table.string("role_name", 64).notNullable();
+    table.integer("user_id").notNullable();
     table.primary(["role_name", "user_id"]);
     table.foreign("role_name").references("rbac_role.name");
     table.foreign("user_id").references("user_identity.id");
