d7a6b25051cbb85c4d41ed5969119a5216600093
Gazbond <dev@gazbond.co.uk>
2019-03-14 20:54:04 +0000

    Refactored and added sql/http logging and error handlers.

--- app.js
+++ app.js
@@ -14,45 +14,90 @@ const favicon = require("serve-favicon");
 const environment = process.env.ENVIRONMENT || "development";
 const config = require("./knexfile")[environment];
 const knex = require("knex")(config);
+// Log SQL.
+knex.on("query", query => {
+  console.log("[SQL]", query.sql);
+});
+// Provide connection to models.
 const { Model } = require("objection");
 Model.knex(knex);
 
 /**
- * Public route and handlers.
+ * Public router.
  */
 const routerPublic = express.Router();
+/**
+ * Public middleware.
+ */
 routerPublic.use(expressLayouts);
 routerPublic.use(
+  // Content-Type: application/x-www-form-urlencoded
   bodyParser.urlencoded({
     extended: false
   })
 );
-routerPublic.use(bodyParser.json());
-// Home:
+/**
+ * Public home page and favicon.
+ */
+routerPublic.use(favicon("public/favicon.ico"));
 routerPublic.get("/", (req, res) => {
-  return res.render("home");
+  res.render("home");
 });
-// Security:
+/**
+ * Public security controller.
+ */
 const SecurityController = require("./public/SecurityController");
 const securityController = new SecurityController();
-securityController.initRoutes(routerPublic);
+routerPublic.use(securityController.initRoutes());
+/**
+ * Public error handler.
+ */
+routerPublic.use((err, req, res, next) => {
+  if (err.status) res.status(err.status);
+  // 500 Internal Server Error
+  else res.status(500);
+  res.render("error", { error: err });
+});
 
 /**
- * API route and handlers.
+ * API router.
  */
 const routerApi = express.Router();
-// User:
+/**
+ * API middleware.
+ */
+routerApi.use(
+  // Content-Type: application/json
+  bodyParser.json()
+);
+/**
+ * API user endpoint.
+ */
 const UserEndpoint = require("./api/UserEndpoint");
 const userEndpoint = new UserEndpoint();
-userEndpoint.initRoutes(routerApi);
+routerApi.use(userEndpoint.initRoutes());
+/**
+ * API error handler.
+ */
+routerApi.use((err, req, res, next) => {
+  if (err.status) res.status(err.status);
+  // 500 Internal Server Error
+  else res.status(500);
+  res.json({
+    error: {
+      name: err.name,
+      message: err.message
+    }
+  });
+});
 
 /**
  * Create Express server.
  */
 const app = express();
+app.use(logger("[HTTP] :method :url :status"));
 app.set("view engine", "ejs");
-app.use(favicon("public/favicon.ico"));
 app.use("/", routerPublic);
 app.use("/api/", routerApi);
 const PORT = 8080;
-app.listen(PORT, () => console.log(`Node app listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Node listening on ${PORT}`));
