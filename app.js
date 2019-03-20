4a64347dfcf542ea471a22d9425cb06dff73006f
Gazbond <dev@gazbond.co.uk>
2019-03-20 08:45:54 +0000

    Refactored logging and error handlers.

--- app.js
+++ app.js
@@ -6,29 +6,37 @@ const flash = require("express-flash");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const chalk = require("chalk").default;
+const sqlFormatter = require("sql-formatter");
 const favicon = require("serve-favicon");
+const { Model, ValidationError } = require("objection");
 
 /**
+ * ------------------------------------------------------
  * Config.
+ * ------------------------------------------------------
  */
 const config = require("./app.conf");
 
 /**
+ * ------------------------------------------------------
  * Knex database with Objection models (db).
+ * ------------------------------------------------------
  */
 const environment = process.env.ENVIRONMENT || "development";
 const db = require("./knexfile")[environment];
 const knex = require("knex")(db);
 // Log SQL.
 knex.on("query", query => {
-  console.log("[SQL]", query.sql);
+  console.log(chalk.green(query.sql));
 });
 // Provide connection to models.
-const { Model } = require("objection");
 Model.knex(knex);
 
 /**
+ * ------------------------------------------------------
  * Public router.
+ * ------------------------------------------------------
  */
 const routerPublic = express.Router();
 /**
@@ -36,7 +44,7 @@ const routerPublic = express.Router();
  */
 routerPublic.use(expressLayouts);
 routerPublic.use(
-  // Content-Type: application/x-www-form-urlencoded
+  // Content-Type: application/x-www-form-urlencoded.
   bodyParser.urlencoded({
     extended: false
   })
@@ -53,19 +61,24 @@ routerPublic.get("/", (req, res) => {
  */
 const SecurityController = require("./controllers/public/SecurityController");
 const securityController = new SecurityController();
-routerPublic.use(securityController.initRoutes());
+routerPublic.use(securityController.initRouter());
 /**
  * Public error handler.
  */
 routerPublic.use((err, req, res, next) => {
-  if (err.status) res.status(err.status);
+  console.log("err: ", err);
+  // HTTP
+  if (err.statusCode) res.status(err.statusCode);
+  else if (err.status) res.status(err.status);
   // 500 Internal Server Error
   else res.status(500);
   res.render("error", { error: err });
 });
 
 /**
+ * ------------------------------------------------------
  * API router.
+ * ------------------------------------------------------
  */
 const routerApi = express.Router();
 /**
@@ -80,13 +93,22 @@ routerApi.use(
  */
 const UserEndpoint = require("./controllers/api/UserEndpoint");
 const userEndpoint = new UserEndpoint();
-routerApi.use(userEndpoint.initRoutes());
+routerApi.use(userEndpoint.initRouter());
 /**
  * API error handler.
  */
 routerApi.use((err, req, res, next) => {
-  if (err.status) res.status(err.status);
-  // 500 Internal Server Error
+  console.log("err: ", err);
+  // Validation
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json({
+      errors: err.data
+    });
+  }
+  // HTTP.
+  if (err.statusCode) res.status(err.statusCode);
+  else if (err.status) res.status(err.status);
+  // 500 Internal Server Error.
   else res.status(500);
   res.json({
     error: {
@@ -97,13 +119,32 @@ routerApi.use((err, req, res, next) => {
 });
 
 /**
+ * ------------------------------------------------------
  * Create Express server.
+ * ------------------------------------------------------
  */
 const app = express();
-app.use(logger("[HTTP] :method :url :status"));
+// Log routes.
+app.use(
+  logger((tokens, req, res) => {
+    const message = [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      "-",
+      tokens["response-time"](req, res),
+      "ms"
+    ].join(" ");
+    return chalk.yellow(message);
+  })
+);
+// View engine.
 app.set("view engine", "ejs");
+// Public routes.
 app.use("/", routerPublic);
+// API routes.
 app.use("/api/", routerApi);
+// Server.
 const PORT = 8080;
 const NAME = config.name;
 app.listen(PORT, () => console.log(`${NAME} listening on ${PORT}`));
