81a2180a35589bea8ea9e3b58060320d385eebfb
Gazbond <dev@gazbond.co.uk>
2019-03-28 18:09:47 +0000

    Updated seeds to reflect auto role assignments.

--- seeds/users.js
+++ seeds/users.js
@@ -8,49 +8,56 @@ exports.seed = async function(knex) {
   await knex("rbac_role").del();
   await knex("user_identity").del();
 
+  /**
+   * Roles/Permissions.
+   */
+  const Role = require("../models/rbac/Role");
+  const adminRole = await Role.query().insertAndFetch({
+    name: "admin"
+  });
+  const userRole = await Role.query().insertAndFetch({
+    name: "user"
+  });
+  const Permission = require("../models/rbac/Permission");
+  const writePerm = await Permission.query().insertAndFetch({
+    name: "can-write-api"
+  });
+  const readPerm = await Permission.query().insertAndFetch({
+    name: "can-read-api"
+  });
+  await adminRole.assignPermissions([writePerm, readPerm]);
+  await userRole.assignPermission(readPerm);
+
+  /**
+   * Root user with additional admin role.
+   */
   const User = require("../models/User");
   const rootUser = await User.query().insertAndFetch({
     username: "root",
     email: "dev@gazbond.co.uk",
     password: "password"
   });
+  await rootUser.assignRole(adminRole);
+
+  /**
+   * Known user with default roles.
+   */
   const gazbondUser = await User.query().insertAndFetch({
     username: "gazbond",
     email: "gaz@gazbond.co.uk",
     password: "password"
   });
-  const Role = require("../models/rbac/Role");
-  const adminRole = await Role.query().insertAndFetch({
-    name: "admin"
-  });
 
-  const userRole = await Role.query().insertAndFetch({
-    name: "user"
-  });
-
-  const Permission = require("../models/rbac/Permission");
-  const writePerm = await Permission.query().insertAndFetch({
-    name: "can-write-api"
-  });
-
-  const readPerm = await Permission.query().insertAndFetch({
-    name: "can-read-api"
-  });
-
-  await adminRole.assignPermissions([writePerm, readPerm]);
-  await userRole.assignPermission(readPerm);
-  await rootUser.assignRoles([adminRole, userRole]);
-  await gazbondUser.assignRole(userRole);
-
-  // Lots more user role users
+  /**
+   * Lots more users.
+   */
   const faker = require("faker");
   const usersCount = 28;
   for (let i = 0; i <= usersCount; i++) {
-    const current = await User.query().insert({
+    await User.query().insert({
       username: faker.internet.userName(),
       email: faker.internet.email(),
       password: "password"
     });
-    await current.assignRole(userRole);
   }
 };
