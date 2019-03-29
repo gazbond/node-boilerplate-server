d90faa5f6792fbaa3d3dcac1d9ff2b540b14b9ac
Gazbond <dev@gazbond.co.uk>
2019-03-29 21:30:45 +0000

    Using type checked equals.

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@
  */
 const environment = process.env.ENVIRONMENT || "development";
 let configPath = "./dev.conf";
-if (environment == "testing") {
+if (environment === "testing") {
   configPath = "./test.conf";
 }
 module.exports = require(configPath);
