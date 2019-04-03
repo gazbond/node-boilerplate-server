592065e3a9b2dbf7c436eb549009b3ac9bec0a07
Gazbond <dev@gazbond.co.uk>
2019-04-03 11:54:04 +0100

    Added me action/route and eager loading roles/permissions.

--- controllers/api/UserEndpoint.js
+++ controllers/api/UserEndpoint.js
@@ -2,9 +2,11 @@ const BaseEndpoint = require("../../library/BaseEndpoint");
 const User = require("../../models/User");
 const passport = require("../../library/helpers/passport");
 const rbac = require("../../library/helpers/rbac");
+const { bindMethods, wrapAsync } = require("../../library/helpers/utils");
 
 /**
  * User endpoint exposes User model over http.
+ * Also provides users/me endpoint for returning owner of jwt token.
  */
 module.exports = class UserEndpoint extends BaseEndpoint {
   constructor() {
@@ -24,8 +26,31 @@ module.exports = class UserEndpoint extends BaseEndpoint {
     this.middleware.create = writeMiddleware;
     this.middleware.update = writeMiddleware;
     this.middleware.delete = writeMiddleware;
+    this.middleware.me = readMiddleware;
+    this.eager = "roles.permissions";
+    bindMethods(this, ["actionMe"]);
   }
+  initRouter() {
+    this.router.get("/users/me", this.middleware.me, wrapAsync(this.actionMe));
+    super.initRouter();
+    return this.router;
+  }
+  /**
+   * GET api/users
+   */
   async actionIndex(req, res) {
     return super.actionIndex(req, res);
+  }
+  /**
+   * GET api/users/me
+   */
+  async actionMe(req, res) {
+    const user = req.user;
+    if (!user) {
+      // 404 Not Found
+      return res.status(404).end();
+    }
+    // // 200 OK
+    res.status(200).send(user);
   }
 };
