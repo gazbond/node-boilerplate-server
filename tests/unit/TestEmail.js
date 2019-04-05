2a380f8f0135e551be31b9eaa8da8c08e9188afe
Gazbond <dev@gazbond.co.uk>
2019-04-05 11:27:23 +0100

    Now checks email json file.

--- tests/unit/TestEmail.js
+++ tests/unit/TestEmail.js
@@ -1,5 +1,5 @@
-process.env.ENVIRONMENT = "testing";
-
+const path = require("path");
+const fs = require("fs");
 const expect = require("expect.js");
 const { transport, sendEmail } = require("../../library/helpers/email");
 
@@ -8,13 +8,27 @@ after(async function() {
 });
 
 describe("Test email helper", function() {
-  it("tests sending email with register template", async function() {
-    const response = await sendEmail("dev@gazbond.co.uk", "register", {
+  it("tests sending email with confirmation template", async function() {
+    await sendEmail("dev@gazbond.co.uk", "confirmation", {
       name: "node-boilerplate-server",
       username: "Gazbond",
       email: "dev@gazbond.co.uk",
       url: "link-goes-here"
     });
-    expect(response).to.have.property("messageId");
+    const file = JSON.parse(
+      await fs.readFileSync(
+        path.resolve(
+          __dirname,
+          "../_output/emails/confirmation/dev@gazbond.co.uk.json"
+        ),
+        "utf8"
+      )
+    );
+    expect(file).to.have.property("to");
+    expect(file).to.have.property("subject");
+    expect(file.to).to.equal("dev@gazbond.co.uk");
+    expect(file.subject).to.equal(
+      "Hi Gazbond, welcome to node-boilerplate-server"
+    );
   });
 });
