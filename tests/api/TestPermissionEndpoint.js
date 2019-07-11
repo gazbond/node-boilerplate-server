7186c820b2d19b7e2c80c93316705dc685dd7309
Gazbond <dev@gazbond.co.uk>
2019-07-11 13:45:20 +0100

    Added Role/Permission endpoint tests and updated User endpoint test to handle assignments.

---
+++ tests/api/TestPermissionEndpoint.js
@@ -0,0 +1,78 @@
+const expect = require("expect.js");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+
+const server = "http://nodetest:7070";
+
+describe("Test PermissionEndpoint", async function() {
+  // Login:
+  let token;
+  it("Login to /security/login", async function() {
+    // Login form returns { Authorization: <token> }
+    const login = await chai
+      .request(server)
+      .post("/security/login")
+      .set("Content-Type", "application/json")
+      .send({
+        login: "root",
+        password: "password"
+      });
+    token = login.body.Authorization;
+  });
+  // Tests:
+  it("test PermissionEndpoint index and view actions", async function() {
+    const response = await chai
+      .request(server)
+      .get("/api/permissions")
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.status).to.equal(200);
+    expect(response.body).to.be.an(Array);
+    for (let i = 0; i < response.body.length; i++) {
+      const name = response.body[i].name;
+      const current = await chai
+        .request(server)
+        .get(`/api/permissions/${name}`)
+        .set("Authorization", `Bearer ${token}`);
+      expect(current.status).to.equal(200);
+      expect(current.body.name).to.equal(name);
+    }
+  });
+  let name;
+  it("test PermissionEndpoint create action", async function() {
+    const response = await chai
+      .request(server)
+      .post("/api/permissions")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        name: "new-permission"
+      });
+    name = response.body.name;
+    expect(response.status).to.equal(200);
+    expect(response.body.name).to.eql("new-permission");
+  });
+  it("test PermissionEndpoint delete action", async function() {
+    let response = await chai
+      .request(server)
+      .delete(`/api/permissions/${name}`)
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.status).to.equal(200);
+    response = await chai
+      .request(server)
+      .get(`/api/permissions/${name}`)
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.status).to.equal(404);
+  });
+  it("test PermissionEndpoint actions with non existant ids", async function() {
+    let response = await chai
+      .request(server)
+      .get(`/api/permissions/${name}`)
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.status).to.equal(404);
+    response = await chai
+      .request(server)
+      .delete(`/api/permissions/${name}`)
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.status).to.equal(404);
+  });
+});
--- tests/api/TestRoleEndpoint.js
+++ tests/api/TestRoleEndpoint.js
@@ -5,7 +5,7 @@ chai.use(chaiHttp);
 
 const server = "http://nodetest:7070";
 
-describe("Test UserEndpoint", async function() {
+describe("Test RoleEndpoint", async function() {
   // Login:
   let token;
   it("Login to /security/login", async function() {
@@ -21,108 +21,93 @@ describe("Test UserEndpoint", async function() {
     token = login.body.Authorization;
   });
   // Tests:
-  it("test UserEndpoint index and view actions", async function() {
+  it("test RoleEndpoint index and view actions", async function() {
     const response = await chai
       .request(server)
-      .get("/api/users")
+      .get("/api/roles")
       .set("Authorization", `Bearer ${token}`);
     expect(response.status).to.equal(200);
     expect(response.body).to.be.an(Array);
-    response.body.forEach(async user => {
-      const id = user.id;
+    for (let i = 0; i < response.body.length; i++) {
+      const name = response.body[i].name;
       const current = await chai
         .request(server)
-        .get(`/api/users/${id}`)
+        .get(`/api/roles/${name}`)
         .set("Authorization", `Bearer ${token}`);
       expect(current.status).to.equal(200);
-      expect(current.body.id).to.equal(id);
-    });
+      expect(current.body.name).to.equal(name);
+    }
   });
-  let id;
-  it("test UserEndpoint create action", async function() {
+  let name;
+  it("test RoleEndpoint create action", async function() {
     const response = await chai
-      .request(server)
-      .post("/api/users")
-      .set("Authorization", `Bearer ${token}`)
-      .send({
-        username: "gazb",
-        email: "me@email.com",
-        password: "password"
-      });
-    id = response.body.id;
-    expect(response.status).to.equal(200);
-    expect(response.body.username).to.eql("gazb");
-    expect(response.body.email).to.eql("me@email.com");
-  });
-  it("test UserEndpoint update action", async function() {
-    const response = await chai
-      .request(server)
-      .put(`/api/users/${id}`)
-      .set("Authorization", `Bearer ${token}`)
-      .send({
-        email: "you@email.com"
-      });
-    expect(response.status).to.equal(200);
-    expect(response.body.username).to.eql("gazb");
-    expect(response.body.email).to.eql("you@email.com");
-  });
-  it("test UserEndpoint assign and remove role", async function() {
-    const roleName = "new-role";
-    let response = await chai
       .request(server)
       .post("/api/roles")
       .set("Authorization", `Bearer ${token}`)
       .send({
-        name: roleName
+        name: "new-role"
       });
+    name = response.body.name;
     expect(response.status).to.equal(200);
-    expect(response.body.name).to.eql(roleName);
-    response = await chai
+    expect(response.body.name).to.eql("new-role");
+  });
+  it("test RoleEndpoint assign and remove permission", async function() {
+    const permissionName = "new-permission";
+    let response = await chai
       .request(server)
-      .post(`/api/users/${id}/role`)
+      .post("/api/permissions")
       .set("Authorization", `Bearer ${token}`)
       .send({
-        name: roleName
+        name: permissionName
       });
     expect(response.status).to.equal(200);
-    let roleNames = [];
-    response.body.roles.forEach(role => roleNames.push(role.name));
-    expect(roleNames).to.contain(roleName);
+    expect(response.body.name).to.eql(permissionName);
     response = await chai
       .request(server)
-      .delete(`/api/users/${id}/role/${roleName}`)
+      .post(`/api/roles/${name}/permission`)
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        name: permissionName
+      });
+    expect(response.status).to.equal(200);
+    let permissionNames = [];
+    response.body.permissions.forEach(permission =>
+      permissionNames.push(permission.name)
+    );
+    expect(permissionNames).to.contain(permissionName);
+    response = await chai
+      .request(server)
+      .delete(`/api/roles/${name}/permission/${permissionName}`)
       .set("Authorization", `Bearer ${token}`);
     expect(response.status).to.equal(200);
-    roleNames = [];
-    response.body.roles.forEach(role => roleNames.push(role.name));
-    expect(roleNames).not.contain(roleName);
+    permissionNames = [];
+    response.body.permissions.forEach(permission =>
+      permissionNames.push(permission.name)
+    );
+    expect(permissionNames).not.contain(permissionName);
   });
-  it("test UserEndpoint delete action", async function() {
+  it("test RoleEndpoint delete action", async function() {
     let response = await chai
       .request(server)
-      .delete(`/api/users/${id}`)
+      .delete(`/api/roles/${name}`)
       .set("Authorization", `Bearer ${token}`);
     expect(response.status).to.equal(200);
     response = await chai
       .request(server)
-      .get(`/api/users/${id}`)
+      .get(`/api/roles/${name}`)
       .set("Authorization", `Bearer ${token}`);
     expect(response.status).to.equal(404);
   });
-  it("test UserEndpoint actions with non existant ids", async function() {
+  it("test RoleEndpoint actions with non existant ids", async function() {
     let response = await chai
       .request(server)
-      .get(`/api/users/${id}`)
+      .get(`/api/roles/${name}`)
       .set("Authorization", `Bearer ${token}`);
+    expect(response.status).to.equal(404);
     expect(response.status).to.equal(404);
     response = await chai
       .request(server)
-      .put(`/api/users/${id}`)
-      .set("Authorization", `Bearer ${token}`);
-    expect(response.status).to.equal(404);
-    response = await chai
-      .request(server)
-      .delete(`/api/users/${id}`)
+      .delete(`/api/roles/${name}`)
       .set("Authorization", `Bearer ${token}`);
     expect(response.status).to.equal(404);
   });
