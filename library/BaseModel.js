e5880fa1fc80fadbed1de6c2b3fa34c8e1212dad
Gazbond <dev@gazbond.co.uk>
2019-07-18 19:13:11 +0100

    Added event handlers for ElasticSearch indices.

--- library/BaseModel.js
+++ library/BaseModel.js
@@ -2,6 +2,8 @@ const { Model } = require("objection");
 const { DbErrors } = require("objection-db-errors");
 const VisibilityPlugin = require("objection-visibility").default;
 const BaseClass = DbErrors(VisibilityPlugin(Model));
+const { getBody } = require("../library/helpers/utils");
+const { elastic } = require("../config");
 
 module.exports = class BaseModel extends BaseClass {
   constructor() {
@@ -25,6 +27,67 @@ module.exports = class BaseModel extends BaseClass {
     await super.$beforeUpdate(opt, queryContext);
     if (this.$autoTimestamps) {
       this.updated_at = new Date().toISOString();
+    }
+  }
+  /**
+   * Insert ElasticSearch index.
+   */
+  async $afterInsert(queryContext) {
+    await super.$afterInsert(queryContext);
+    // @ts-ignore
+    const index = this.constructor.indexName;
+    if (index !== undefined) {
+      // @ts-ignore
+      const type = this.constructor.indexType || null;
+      // Insert index
+      let body = getBody(this.constructor, this);
+      await elastic.index({
+        index: index,
+        type: type,
+        id: this.id,
+        body: body,
+        refresh: true
+      });
+    }
+  }
+  /**
+   * Update ElasticSearch index.
+   */
+  async $afterUpdate(opt, queryContext) {
+    await super.$afterUpdate(opt, queryContext);
+    // @ts-ignore
+    const index = this.constructor.indexName;
+    if (index !== undefined) {
+      // @ts-ignore
+      const type = this.constructor.indexType || null;
+      // Update index
+      let body = getBody(this.constructor, this, true);
+      await elastic.index({
+        index: index,
+        type: type,
+        id: this.id,
+        body: body,
+        refresh: true
+      });
+    }
+  }
+  /**
+   * Delete ElasticSearch index.
+   */
+  async $afterDelete(queryContext) {
+    await super.$afterDelete(queryContext);
+    // @ts-ignore
+    const index = this.constructor.indexName;
+    if (index !== undefined) {
+      // @ts-ignore
+      const type = this.constructor.indexType || null;
+      // Delete index
+      await elastic.delete({
+        index: index,
+        type: type,
+        id: this.id,
+        refresh: true
+      });
     }
   }
 };
